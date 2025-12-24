2824d91decbf332b5f26ae81636bafe2e1136fdc
Racheal5777 <rachealtifr@gmail.com>
2025-12-24 12:15:20 -0800

    frontend

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ form.addEventListener('submit', async (e) => {
         });
         const data = await response.json();
         console.log(data);
-        alert('Submission Successful🎉')
+        alert('Submission Successful🎉');
     } catch (error) {
         console.error(error);
     }
